83b17fed3ff5de07222e6c539655c9cd98a298e9
sunny-nie <2932384504@qq.com>
2026-03-22 22:32:19 +0800

    add fs-rm nodes and update README.md

--- README.md
+++ README.md
@@ -1,7 +1,51 @@
-## package
+## Intro
+- This package is suitable for using the fs base API in node-red
+- 此包适用于在node-red中使用fs基础api
+
+## Package
 node-red-contrib-fs-base
 
-## usage
+## Install
+### ``二选一``
+
+- ### Dashboard Install
+![install](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/install.png)
+
+- ### Console Install
 ```javascript
+cd ~/.node-red
 npm install node-red-contrib-fs-base
 ```
+
+## Examples
+### 1. fs-read-dir
+### NodeJs API: [``fs.readdirSync``](https://nodejs.cn/api/fs.html#fsreaddirsyncpath-options)
+### ``读取指定目录资源``
+
+![fs-read-dir](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-read-dir.png)
+
+![fs-read-dir-example1](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-read-dir-1.png)
+
+### 2. fs-exists
+### NodeJs API: [``fs.existsSync``](https://nodejs.cn/api/fs.html#fsexistssyncpath)
+### ``检测指定资源是否存在``
+
+![fs-exists](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-exists.png)
+
+![fs-exists-example1](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-exists-1.png)
+
+### 3. fs-unlink
+### NodeJs API: [``fs.unlinkSync``](https://nodejs.cn/api/fs.html#fsunlinksyncpath)
+### ``删除指定文件``
+
+![fs-unlink](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-unlink.png)
+
+![fs-unlink-example1](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-unlink-1.png)
+
+### 4. fs-rm
+### NodeJs API: [``fs.rmSync``](https://nodejs.cn/api/fs.html#fsrmsyncpath-options)
+### ``删除指定资源``
+
+![fs-unlink](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-rm.png)
+
+![fs-unlink-example1](https://raw.githubusercontent.com/sunny-nie/image-store/main/node/node-red-plugins/node-red-contrib-fs-base/fs-rm-1.png)
---
+++ nodes/function/fs-rm.js
@@ -0,0 +1,142 @@
+var fs = require("fs");
+var _ = require("lodash")
+
+module.exports = function (RED) {
+    // 同步删除文件和目录
+    function FsRm(config) {
+        RED.nodes.createNode(this, config);
+        this.name = config.name;
+        this.path = config.path;
+        this.pathType = config.pathType;
+        this.recursive = config.recursive;
+        this.recursiveType = config.recursiveType;
+        this.maxRetries = config.maxRetries;
+        this.maxRetriesType = config.maxRetriesType;
+        this.retryDelay = config.retryDelay;
+        this.retryDelayType = config.retryDelayType;
+        var node = this;
+        node.on('input', function (msg, send, done) {
+            // prev node's msg
+            node.warn(msg);
+            // this node info
+            console.log(node);
+
+            // msg.payload = [];
+            var result = {
+                result: false,
+                message: ""
+            };
+
+            var path = undefined;
+            if (node.pathType == "str") {
+                path = node.path;
+            } else if (node.pathType == "msg") {
+                path = _.get(msg, node.path, undefined);
+                console.log(path);
+            } else if (node.pathType == "flow") {
+                path = node.context().flow.get(node.path);
+                console.log(path);
+            } else if (node.pathType == "global") {
+                path = node.context().global.get(node.path);
+            } else {
+                node.status({ fill: "red", shape: "dot", text: "fs-rm.info.select" });
+                return;
+            }
+            console.log(`path: ${path}`);
+
+            if (path == undefined) {
+                node.warn(RED._("fs-rm.info.select"));
+                node.status({ fill: "red", shape: "dot", text: "fs-rm.info.select" });
+                return;
+            }
+            var stat = fs.statSync(path, { throwIfNoEntry: false });
+            if (stat == undefined) {
+                node.status({ fill: "red", shape: "dot", text: "fs-rm.info.select" });
+                return;
+            } else {
+                node.status({});
+            }
+
+            // 是否递归
+            var recursive = undefined;
+            if (node.recursiveType == "bool") {
+                recursive = node.recursive;
+            } else if (node.recursiveType == "msg") {
+                recursive = _.get(msg, node.recursive, false);
+                console.log(recursive);
+            } else if (node.recursiveType == "flow") {
+                recursive = node.context().flow.get(node.recursive);
+                console.log(recursive);
+            } else if (node.recursiveType == "global") {
+                recursive = node.context().global.get(node.recursive);
+            } else {
+                recursive = false;
+            }
+            recursive = Boolean(recursive);
+            console.log(`recursive: ${recursive}`);
+
+            
+            // 重试次数
+            var maxRetries = undefined;
+            if (node.maxRetriesType == "num") {
+                maxRetries = node.maxRetries;
+            } else if (node.maxRetriesType == "msg") {
+                maxRetries = _.get(msg, node.maxRetries, 0);
+                console.log(maxRetries);
+            } else if (node.maxRetriesType == "flow") {
+                maxRetries = node.context().flow.get(node.maxRetries);
+                console.log(maxRetries);
+            } else if (node.maxRetriesType == "global") {
+                maxRetries = node.context().global.get(node.maxRetries);
+            } else {
+                maxRetries = 0;
+            }
+            // 设置重试次数为自然数
+            maxRetries = Number.parseInt(maxRetries);
+            if (Number.isNaN(maxRetries) || maxRetries < 0) {
+                maxRetries = 0;
+            }
+            console.log(`maxRetries: ${maxRetries}`);
+
+            // 重试时间
+            var retryDelay = undefined;
+            if (node.retryDelayType == "num") {
+                retryDelay = node.retryDelay;
+            } else if (node.retryDelayType == "msg") {
+                retryDelay = _.get(msg, node.retryDelay, 100);
+                console.log(retryDelay);
+            } else if (node.retryDelayType == "flow") {
+                retryDelay = node.context().flow.get(node.retryDelay);
+                console.log(retryDelay);
+            } else if (node.retryDelayType == "global") {
+                retryDelay = node.context().global.get(node.retryDelay);
+            } else {
+                node.status({ fill: "red", shape: "dot", text: "fs-rm.info.select" });
+                return;
+            }
+            // 值无效或者值小于100, 则设置默认值为100
+            retryDelay = Number.parseInt(retryDelay);
+            if (Number.isNaN(retryDelay) || retryDelay < 100) {
+                retryDelay = 100;
+            }
+            console.log(`retryDelay: ${retryDelay}`);
+
+            try {
+                fs.rmSync(path, { recursive, maxRetries, retryDelay });
+                result.result = true;
+                result.message = "remove path success!";
+            } catch (error) {
+                result.result = false;
+                result.message = error.message;
+                node.error(error);
+            }
+
+            msg.payload = result;
+            send(msg);
+            if (done) {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("fs rm", FsRm);
+}
